1e953921f5f1cf367f504bdc6f3b4b4c55a252c8
FlutLab.io <>
2022-03-09 03:41:32 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -469,7 +469,7 @@ class _NewActivityPage extends MaterialPageRoute<void> {
                           selectedItem: _selectedCourse,
                           onChanged: (value) => _selectedCourse = value,
                         ).then((selection) =>
-                        print(selection));
+                        print(selection!.toID()));
                       }
                     ),
                     const SizedBox(width: 5),
