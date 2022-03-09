b137211226c39a7e92b28b008f6f9eed488ff0fd
FlutLab.io <>
2022-03-09 06:49:01 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -475,7 +475,7 @@ class _NewActivityPage extends MaterialPageRoute<void> {
                     ),
                     const SizedBox(width: 5),
                     Flexible(child: TextFormField(
-                          initialValue:  _selectedCourse.toString(),
+                          initialValue:  '',
                           showCursor: true,
                           onChanged: (String value) => _courseName = value,
                           //keyboardType: TextInputType.name,
