f299eabf55453a21be749ede7256e5fe397b70ad
FlutLab.io <>
2022-03-11 08:40:40 +0000

    Autosave

--- lib/CreatePage.dart
+++ lib/CreatePage.dart
@@ -269,8 +269,8 @@ class _NewGolfCoursePage extends MaterialPageRoute<bool> {
                           {"title": "9", 'index': 10,'key': 'h9'},
                         ], 
                         rows: [
-                          {'zoName': 'Out', 'h1': '', 'h2': '', 'h3': '', 'h4': '', 'h5': '', 'h6': '', 'h7': '', 'h8': '', 'h9': ''},
-                          {'zoName': 'In','h1': '','h2': '', 'h3': '', 'h4': '', 'h5': '', 'h6': '', 'h7': '', 'h8': '', 'h9': ''},
+                          {'zoName': 'Ou', 'h1': '', 'h2': '', 'h3': '', 'h4': '', 'h5': '', 'h6': '', 'h7': '', 'h8': '', 'h9': ''},
+                          {'zoName': 'I','h1': '','h2': '', 'h3': '', 'h4': '', 'h5': '', 'h6': '', 'h7': '', 'h8': '', 'h9': ''},
                         ]
                       )),
                       const SizedBox(height: 16.0),
