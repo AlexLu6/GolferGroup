1a1b312c22daaa462dd0700ee98caecf3f33a3bc
FlutLab.io <>
2022-03-11 08:38:44 +0000

    Autosave

--- lib/CreatePage.dart
+++ lib/CreatePage.dart
@@ -252,7 +252,7 @@ class _NewGolfCoursePage extends MaterialPageRoute<bool> {
                         showSaveIcon: true,
                         saveIcon: Icons.save,
                         saveIconColor: Colors.blue,
-                        onRowSaved: (row) => print(row),
+                        onRowSaved: (row) => print(row['zoName']),
                         showCreateButton: true,
                         createButtonLabel: Text('Add zone'),
                         createButtonIcon: Icon(Icons.add), createButtonColor: Colors.blue,
