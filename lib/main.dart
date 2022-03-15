2c4ec22908289050fed4f54ba099b0da51849073
FlutLab.io <>
2022-03-15 06:03:22 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -400,7 +400,7 @@ class _MyHomePageState extends State<MyHomePage> {
         leading: Image.network(coursePhoto(golferActivities.elementAt(index)["cid"] as int)!), 
         trailing: Icon(Icons.keyboard_arrow_right),
         onTap: () {
-          Navigator.push(context, showActivityPage(golferActivities.elementAt(index), _golferID, golferName(_golferID)!, _golferID == ));
+          Navigator.push(context, showActivityPage(golferActivities.elementAt(index), _golferID, golferName(_golferID)!, _golferID == golferActivities.elementAt(index)['uid'] as int));
         }
       ),
       separatorBuilder: (context, index) => Divider(),
