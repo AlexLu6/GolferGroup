612bb29a0849bd23b2ef919c4809bfd1b5bd3571
FlutLab.io <>
2022-03-18 11:49:03 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:firestore_core/firestore_core.dart';
 import 'dataModel.dart';
 import 'CreatePage.dart';
 import 'locale/language.dart';
