c7d34c8d68d1fc686c74f9f0ea41b734e0caec34
FlutLab.io <>
2022-03-24 08:35:34 +0000

    Autosave

--- lib/dataModel.dart
+++ lib/dataModel.dart
@@ -285,18 +285,27 @@ var golfCourses = {
 };
 
 String? courseName(int cid) {
-  var result = '';
-  for (var e in golfCourses) 
-    if (e["cid"] == cid) 
-      return (e["region"] as String) + ' ' + (e["name"] as String);
-  return result;
+  FirebaseFirestore.instance.collection('GolfCourses')
+    .where('cid', isEqualTo: cid)
+    .get().then((value) {
+      value.docs.forEach((result) {
+          var items = result.data();
+          return items['region'] + ' ' + items['Name'];
+      });
+    });
+  return '';
 }
 
 String? coursePhoto(int cid) {
-  var result = '';
-  for (var e in golfCourses) 
-    if (e["cid"] == cid) 
-      return (e["photo"] as String);
+  var result = 'https://images.unsplash.com/photo-1623567341691-1f47b5cf949e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80';
+  FirebaseFirestore.instance.collection('GolfCourses')
+    .where('cid', isEqualTo: cid)
+    .get().then((value) {
+      value.docs.forEach((result) {
+          var items = result.data();
+          return items['Photo'];
+      });
+    });
   return result;
 }
 
