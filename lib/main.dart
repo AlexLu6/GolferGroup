8a42ea7f435600f4445b03fbcfeae0a42488a8af
FlutLab.io <>
2022-03-25 06:23:59 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -429,7 +429,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: snapshot.data!.docs.map((doc) {
               return Card(child: ListTile(
                 title: Text(courseName((doc.data()! as Map)['cid'] as int)!, style: TextStyle(fontSize: 20)),
-                subtitle: Text(Language.of(context).teeOff + ((doc.data()! as Map)['teeOff']).toDateTime() + '\n' +
+                subtitle: Text(Language.of(context).teeOff + ((doc.data()! as Map)['teeOff']).toDate() + '\n' +
                   Language.of(context).max + (doc.data()! as Map)['max'].toString() + '\t' + 
                   Language.of(context).now + ((doc.data()! as Map)['golfers'] as List).length.toString() + "\t" + 
                   Language.of(context).fee + (doc.data()! as Map)['fee'].toString()),
