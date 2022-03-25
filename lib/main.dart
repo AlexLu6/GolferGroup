0cdcb80d8fd61c1bb6f5589e4dc678b3a65fc5e8
FlutLab.io <>
2022-03-25 08:06:28 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -457,9 +457,9 @@ class _MyHomePageState extends State<MyHomePage> {
           return ListView(
             children: snapshot.data!.docs.map((doc) {
               return Card(child: ListTile(
-                title: Text((doc.data()! as Map)["region"] + (doc.data()! as Map)["name"], style: TextStyle(fontSize: 20)),
+                leading: Image.network((doc.data()! as Map)["photo"]),
+                title: Text((doc.data()! as Map)["region"] + ' ' + (doc.data()! as Map)["name"], style: TextStyle(fontSize: 20)),
                 subtitle: Text((((doc.data()! as Map)["zones"]).length * 9).toString() + ' Holes'), 
-                leading: Image.network((doc.data()! as Map)["photo"]), 
                 trailing: Icon(Icons.keyboard_arrow_right)
               ));
             }).toList()
