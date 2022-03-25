c32ee18aac62b82ea9d5c58b82321cada09250a8
FlutLab.io <>
2022-03-25 06:52:59 +0000

    Autosave

--- lib/dataModel.dart
+++ lib/dataModel.dart
@@ -298,7 +298,7 @@ String? courseName(int cid) {
           if ((items['cid'] as int) == cid)
             res = (items['region'] as String)+ ' ' + (items['name'] as String);
       });
-    }).whenComplete(() => res);
+    }).whenComplete(() {return res;});
 }
 
 String? coursePhoto(int cid) {
