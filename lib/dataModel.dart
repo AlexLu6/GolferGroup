430e66a9dde3997270ed9a87715fe1295ca042fd
FlutLab.io <>
2022-04-01 01:06:43 +0000

    Autosave

--- lib/dataModel.dart
+++ lib/dataModel.dart
@@ -156,7 +156,7 @@ Future <String>? golferName(int uid) {
 Future <String>? golferNames(List<dynamic> uids) {
   var res;
   return FirebaseFirestore.instance.collection('Golfers')
-    .where('uid', isEqualTo: uids)
+    .where('uid', arrayContainsAny: uids)
     .get().then((value) {
       value.docs.forEach((result) {
           var items = result.data();
@@ -175,7 +175,7 @@ String? golferNames(List<dynamic> uids) {
       result = result == null ? name : result + ', ' + name;
       print(result);
   });
-  print('now: '+ result);
+  print('now: '+ result?? 'NoBody');
   return result?? 'NoBody';
 }
 
