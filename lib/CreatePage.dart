1f4e0fc234e98eecdbf920fdfc6769f9a813ec99
FlutLab.io <>
2022-04-15 08:11:14 +0000

    Autosave

--- lib/CreatePage.dart
+++ lib/CreatePage.dart
@@ -492,10 +492,12 @@ class _NewScorePage extends MaterialPageRoute<bool> {
           )),
           const SizedBox(height: 6.0),
           Text('Total: $sum1 + $sum2 = ' + (sum1+sum2).toString(), style: TextStyle(fontSize: 20)),
+          const SizedBox(height: 16.0),
+          Center(child:
           ElevatedButton(
             child: Text(Language.of(context).store),
             onPressed: () => Navigator.of(context).pop(true)
-          )
+          ))
         ]));
       })
     );
